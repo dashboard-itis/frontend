a0d86ff5ddd050bafb69325ade07693429a7e72a
arinasidr <sidorinaarina4@gmail.com>
2026-03-16 23:43:00 +0300

    add routing & FSD architecture

---
+++ src/app/providers/router/RoleRoute.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+
+export const RoleRoute = ({ children }: Props) => {
+  return children
+}
---
+++ src/pages/curator-dashboard-page/ui/CuratorDashboardPage.tsx
@@ -0,0 +1,3 @@
+export const CuratorDashboardPage = () => {
+  return <div>Curator Dashboard</div>
+}

6e07f4d32e457252a368584c237ceaaa4f57d086
arinasidr <sidorinaarina4@gmail.com>
2026-03-18 15:25:17 +0300

    добавила в пустые файлы импорты, чтобы тайпскрипт не ругался и используем craco для настройки alias

--- src/app/providers/router/router.tsx
+++ src/app/providers/router/router.tsx
@@ -3,11 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { PrivateRoute } from './ProtectedRoute'
 import { RoleRoute } from './RoleRoute'
 
-import { AdminDashboardPage } from '../../../pages/admin-dashboard-page/ui/AdminDashboardPage'
-import { CuratorDashboardPage } from '../../../pages/curator-dashboard-page/ui/CuratorDashboardPage'
-import { LoginPage } from '../../../pages/login-page/ui/LoginPage'
-import { RegisterPage } from '../../../pages/register-page/ui/RegisterPage'
-import { StudentDashboardPage } from '../../../pages/student-dashboard-page/ui/StudentDashboardPage'
+import { AdminDashboardPage } from '@/pages/admin-dashboard-page/ui/AdminDashboardPage'
+import { CuratorDashboardPage } from '@/pages/curator-dashboard-page/ui/CuratorDashboardPage'
+import { LoginPage } from '@/pages/login-page/ui/LoginPage'
+import { RegisterPage } from '@/pages/register-page/ui/RegisterPage'
+import { StudentDashboardPage } from '@/pages/student-dashboard-page/ui/StudentDashboardPage'
 
 export const AppRouter = () => {
   return (

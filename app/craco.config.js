a18f1a8614b7414bd4485e8855c184ff67671449
Maxim Sidorin <si.max@Maxims-MacBook-Air.local>
2026-05-29 13:36:00 +0300

    feat(ci/cd): test workflow

--- app/craco.config.js
+++ app/craco.config.js
@@ -6,4 +6,16 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  jest: {
+    configure: {
+      testEnvironment: 'jsdom',
+      reporters: [
+        'default',
+        ['jest-junit', { outputDirectory: '.', outputName: 'junit-report.xml' }],
+      ],
+      collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+      coverageReporters: ['text', 'json', 'json-summary', 'cobertura'],
+      coverageDirectory: './coverage',
+    },
+  },
 }
